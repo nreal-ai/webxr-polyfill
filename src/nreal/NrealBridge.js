7c70256ce5a3aa0e3963fce7e303ab080d1aa5b9
work <work@nreal.ai>
2021-08-16 17:54:48 +0800

    double speed in rotation with touching

--- src/nreal/NrealBridge.js
+++ src/nreal/NrealBridge.js
@@ -1,4 +1,5 @@
-import {vec3, mat4 } from "gl-matrix/src/gl-matrix";
+import { vec3, mat4, quat } from "gl-matrix/src/gl-matrix";
+
 import GLOBAL from '../lib/global';
 
 // {
@@ -25,7 +26,7 @@ function prepareForNextFrameAndCallback(frame_data) {
 
     g_frame_data_state = 1;
     g_frame_data_count++;
-    return window.nrbridge.requestUpdate()?'Y':'N';    
+    return window.nrbridge.requestUpdate() ? 'Y' : 'N';
 }
 
 
@@ -80,6 +81,10 @@ export default class NrealBridge {
 
         window.prepareForNextFrameAndCallback = prepareForNextFrameAndCallback;
 
+        this.touchStartEuler = vec3.create();
+        this.touchstartQuat = quat.create();
+        this.firstTouch = false;
+
     }
 
     _initializeProjectionMatrix(out, fov, near, far) {
@@ -87,138 +92,234 @@ export default class NrealBridge {
     }
 
 
-    startSession(){
+    startSession() {
         this.provider.StartXR();
     }
 
-    endSession(){
+    endSession() {
         this.provider.ExitXR();
     }
-    needUpdate(){
+    needUpdate() {
         if (this.provider === undefined && g_frame_data_state === 0) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
 
-    // request update with phone controller
-    // requestUpdate() {
-
-    //     if(this.animationCallback === null){
-    //         return false;
-    //     }
-
-    //     this.headPose = mat4.clone(g_frame_data);
-
-    //     // update view matrix
-    //     mat4.invert(this.leftViewMatrix, mat4.multiply(this.leftViewMatrix, this.headPose, this.eyeOffset.left));
-    //     mat4.invert(this.rightViewMatrix, mat4.multiply(this.rightViewMatrix, this.headPose, this.eyeOffset.right));
 
 
-    //     if (this.gamepads.length != g_controller_data.count) {
-    //         this._initializeControllers(g_controller_data.count);
-    //     }
-
-    //     for (let i = 0; i < this.gamepads.length; i++) {
-    //         let gamepad = this.gamepads[i];
-    //         gamepad.timestamp= startDate + (performance.now() - startPerfNow) 
-
-
-    //         let data = g_controller_data.data[i];
-    //         let touched = data[0] === 1;
-    //         let pressed = data[1] === 1;
-
-    //         if (touched && Math.abs(gamepad.axes[1]) > 0.01) {
-    //             let offset = data[11] - gamepad.axes[1];
-    //             this.armLength += offset;
-    //             this.armLength = Math.max(0.1, Math.min(2, this.armLength));
-    //         }
-
-
-    //         // touch area
-    //         for (let j = 0; j <gamepad.buttons.length;j++){
-    //             gamepad.buttons[j].touched = false;
-    //             gamepad.buttons[j].pressed = false;
-    //             gamepad.buttons[j].value = 0;
-    //         }
-    //         if(data[11] < -0.5){
-    //             if(data[10] < 0){
-    //                 gamepad.buttons[0].touched = touched;
-    //                 gamepad.buttons[0].pressed = pressed;
-    //                 gamepad.buttons[0].value = pressed ? 1.0 : 0.0;
-
-    //             }else{
-    //                 gamepad.buttons[1].touched = touched;
-    //                 gamepad.buttons[1].pressed = pressed;
-    //                 gamepad.buttons[1].value = pressed ? 1.0 : 0.0;
-    //             }
-    //         }
-
-    //         gamepad.pose.position = data.slice(3, 6);
-    //         gamepad.pose.orientation = data.slice(6, 10);
-    //         gamepad.axes = [data[10], data[11]]
-    //         let arm = vec3.fromValues(0, 0, -this.armLength);
-    //         vec3.transformQuat(arm, arm, gamepad.pose.orientation);
-    //         vec3.add(gamepad.pose.position, gamepad.pose.position, arm);
-    //     }
-
-    //     this.animationCallback();
-
-    //     return true;
-    // }
-
-    requestUpdate(){
-        if(this.animationCallback === null){
+    requestUpdate() {
+        if (this.animationCallback === null) {
             return false;
         }
-
         this.headPose = mat4.clone(g_frame_data);
-
-        // update view matrix
         mat4.invert(this.leftViewMatrix, mat4.multiply(this.leftViewMatrix, this.headPose, this.eyeOffset.left));
         mat4.invert(this.rightViewMatrix, mat4.multiply(this.rightViewMatrix, this.headPose, this.eyeOffset.right));
-
-
         if (this.gamepads.length != g_controller_data.count) {
             this._initializeControllers(g_controller_data.count);
         }
 
         for (let i = 0; i < this.gamepads.length; i++) {
             let gamepad = this.gamepads[i];
-            gamepad.timestamp= startDate + (performance.now() - startPerfNow) 
-
-
+            gamepad.timestamp = startDate + (performance.now() - startPerfNow);
             let data = g_controller_data.data[i];
             let touched = data[0] === 1;
             let pressed = data[1] === 1;
 
-            // if (touched && Math.abs(gamepad.axes[1]) > 0.01) {
-            //     let offset = data[11] - gamepad.axes[1];
-            //     this.armLength += offset;
-            //     this.armLength = Math.max(0.1, Math.min(2, this.armLength));
-            // }
+            let axes0 = data[10];
+            let axes1 = data[11];
 
 
-            // touch area
-            for (let j = 0; j <gamepad.buttons.length;j++){
-                gamepad.buttons[j].touched = touched;
-                gamepad.buttons[j].pressed = pressed;
-                gamepad.buttons[j].value = pressed ? 1.0 : 0.0;
+
+            if (touched && Math.abs(gamepad.axes[1]) > 0.01) {
+                let offset = axes1 - gamepad.axes[1];
+                this.armLength += offset;
+                this.armLength = Math.max(0.01, Math.min(0.75, this.armLength));
             }
-            
 
-            gamepad.pose.position = data.slice(3, 6);
-            gamepad.pose.orientation = data.slice(6, 10);
-            gamepad.axes = [data[10], data[11]]
+            for (let j = 0; j < gamepad.buttons.length; j++) {
+                gamepad.buttons[j].touched = false;
+                gamepad.buttons[j].pressed = false;
+                gamepad.buttons[j].value = 0;
+            }
+
+            let position = data.slice(3, 6);
+            let orientation = data.slice(6, 10);
+
+            if (axes0 > 0.5) {
+                gamepad.buttons[0].touched = touched;
+                gamepad.buttons[0].pressed = pressed;
+                gamepad.buttons[0].value = pressed ? 1.0 : 0.0;
+                gamepad.buttons[1].touched = touched;
+                gamepad.buttons[1].pressed = pressed;
+                gamepad.buttons[1].value = pressed ? 1.0 : 0.0;
+                gamepad.buttons[3].touched = touched;
+                gamepad.buttons[3].pressed = pressed;
+                gamepad.buttons[3].value = pressed ? 1.0 : 0.0;
+            }
+
+            let arm = vec3.fromValues(0, 0, -this.armLength);
+            vec3.transformQuat(arm, arm, orientation);
+            vec3.add(gamepad.pose.position, position, arm);
+
+
+            let handOri = quat.clone(orientation);
+            if (touched) {
+                if (!this.firstTouch) {
+                    this.firstTouch = true;
+                    this.touchstartQuat = quat.clone(orientation);
+                    this._getEuler(this.touchStartEuler, orientation);
+                }
+
+                let curEuler = vec3.create();
+                this._getEuler(curEuler, orientation);
+                
+                let newEuler = vec3.create();
+                newEuler[0] = this.touchStartEuler[0] + (curEuler[0] -  this.touchStartEuler[0])*2;
+                newEuler[1] = this.touchStartEuler[1] + (curEuler[1] -  this.touchStartEuler[1])*2;
+                newEuler[2] = curEuler[2] ;
+
+                
+                const order = 'yxz';
+                let  temp = quat.create();
+                this._fromEuler(temp,newEuler[0],newEuler[1],newEuler[2],order);
+                handOri[0] = temp[3];
+                handOri[1] = temp[2];
+                handOri[2] = temp[0];
+                handOri[3] = temp[1];
+
+
+            } else {
+                this.firstTouch = false;
+            }
+
+
+            gamepad.pose.orientation = handOri;
+            gamepad.axes = [axes0, axes1];
         }
-
         this.animationCallback();
-
         return true;
-
     }
 
+
+    /**
+     * Returns an euler angle representation of a quaternion
+     * @param  {vec3} out Euler angles, pitch-yaw-roll
+     * @param  {quat} mat Quaternion
+     * @return {vec3} out
+     */
+    _getEuler(out, quat) {
+        let x = quat[0],
+            y = quat[1],
+            z = quat[2],
+            w = quat[3],
+            x2 = x * x,
+            y2 = y * y,
+            z2 = z * z,
+            w2 = w * w;
+        let unit = x2 + y2 + z2 + w2;
+        let test = x * w - y * z;
+        if (test > 0.499995 * unit) { //TODO: Use glmatrix.EPSILON
+            // singularity at the north pole
+            out[0] = Math.PI / 2;
+            out[1] = 2 * Math.atan2(y, x);
+            out[2] = 0;
+        } else if (test < -0.499995 * unit) { //TODO: Use glmatrix.EPSILON
+            // singularity at the south pole
+            out[0] = -Math.PI / 2;
+            out[1] = 2 * Math.atan2(y, x);
+            out[2] = 0;
+        } else {
+            out[0] = Math.asin(2 * (x * z - w * y));
+            out[1] = Math.atan2(2 * (x * w + y * z), 1 - 2 * (z2 + w2));
+            out[2] = Math.atan2(2 * (x * y + z * w), 1 - 2 * (y2 + z2));
+        }
+        // TODO: Return them as degrees and not as radians
+
+        let toDegree = 180/ Math.PI;
+
+
+        out[0] *= toDegree
+        out[1] *= toDegree
+        out[2] *= toDegree
+        return out;
+    }
+
+
+    /**
+     * Creates a quaternion from the given euler angle x, y, z using the provided intrinsic order for the conversion.
+     *
+     * @param {quat} out the receiving quaternion
+     * @param {x} x Angle to rotate around X axis in degrees.
+     * @param {y} y Angle to rotate around Y axis in degrees.
+     * @param {z} z Angle to rotate around Z axis in degrees.
+     * @param {'zyx'|'xyz'|'yxz'|'yzx'|'zxy'|'zyx'} order Intrinsic order for conversion, default is zyx.
+     * @returns {quat} out
+     * @function
+     */
+    _fromEuler(out, x, y, z, order = 'xyz') {
+    let halfToRad = Math.PI / 360;
+    x *= halfToRad;
+    z *= halfToRad;
+    y *= halfToRad;
+  
+    let sx = Math.sin(x);
+    let cx = Math.cos(x);
+    let sy = Math.sin(y);
+    let cy = Math.cos(y);
+    let sz = Math.sin(z);
+    let cz = Math.cos(z);
+  
+    switch (order) {
+      case "xyz":
+        out[0] = sx * cy * cz + cx * sy * sz;
+        out[1] = cx * sy * cz - sx * cy * sz;
+        out[2] = cx * cy * sz + sx * sy * cz;
+        out[3] = cx * cy * cz - sx * sy * sz;
+        break;
+  
+      case "xzy":
+        out[0] = sx * cy * cz - cx * sy * sz;
+        out[1] = cx * sy * cz - sx * cy * sz;
+        out[2] = cx * cy * sz + sx * sy * cz;
+        out[3] = cx * cy * cz + sx * sy * sz;
+        break;
+  
+      case "yxz":
+        out[0] = sx * cy * cz + cx * sy * sz;
+        out[1] = cx * sy * cz - sx * cy * sz;
+        out[2] = cx * cy * sz - sx * sy * cz;
+        out[3] = cx * cy * cz + sx * sy * sz;
+        break;
+  
+      case "yzx":
+        out[0] = sx * cy * cz + cx * sy * sz;
+        out[1] = cx * sy * cz + sx * cy * sz;
+        out[2] = cx * cy * sz - sx * sy * cz;
+        out[3] = cx * cy * cz - sx * sy * sz;
+        break;
+  
+      case "zxy":
+        out[0] = sx * cy * cz - cx * sy * sz;
+        out[1] = cx * sy * cz + sx * cy * sz;
+        out[2] = cx * cy * sz + sx * sy * cz;
+        out[3] = cx * cy * cz - sx * sy * sz;
+        break;
+  
+      case "zyx":
+        out[0] = sx * cy * cz - cx * sy * sz;
+        out[1] = cx * sy * cz + sx * cy * sz;
+        out[2] = cx * cy * sz - sx * sy * cz;
+        out[3] = cx * cy * cz + sx * sy * sz;
+        break;
+  
+      default:
+        throw new Error('Unknown angle order ' + order);
+    }
+  
+    return out;
+  }
 
 
     _initializeControllers(controllerNum) {
@@ -249,8 +350,8 @@ export default class NrealBridge {
             buttons: buttons,
             hand: hand,
             mapping: 'xr-standard',
-            axes: [0, 0,0,0],
-            timestamp: startDate + (performance.now() - startPerfNow) 
+            axes: [0, 0, 0, 0],
+            timestamp: startDate + (performance.now() - startPerfNow)
         };
     };
 
@@ -318,13 +419,13 @@ export default class NrealBridge {
     }
 
 
-    updateVrPose(pose){
-        mat4.getTranslation(pose.position,this.headPose);
-        mat4.getRotation(pose.orientation,this.headPose);
+    updateVrPose(pose) {
+        mat4.getTranslation(pose.position, this.headPose);
+        mat4.getRotation(pose.orientation, this.headPose);
     }
 
 
-    updateVrFrameData(frameData){
+    updateVrFrameData(frameData) {
         frameData.leftProjectionMatrix = this.leftProjectionMatrix;
         frameData.leftViewMatrix = this.leftViewMatrix;
         frameData.rightProjectionMatrix = this.rightProjectionMatrix;
@@ -338,10 +439,10 @@ export default class NrealBridge {
 
 var Bridge;
 (
-    function(){
+    function () {
         var instance;
-        Bridge = function Bridge(){
-            if(instance){
+        Bridge = function Bridge() {
+            if (instance) {
                 return instance;
             }
             instance = new NrealBridge();
